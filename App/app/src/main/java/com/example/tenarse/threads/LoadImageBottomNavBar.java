318b48ead4102d397d4fe196331e73ab2ba5b66d
Narcís Gómez Carretero <78963802+A19Narcis@users.noreply.github.com>
2023-04-18 18:08:30 +0200

    Imagenes en circulo BottomNavBar

--- App/app/src/main/java/com/example/tenarse/threads/LoadImageBottomNavBar.java
+++ App/app/src/main/java/com/example/tenarse/threads/LoadImageBottomNavBar.java
@@ -3,17 +3,30 @@ package com.example.tenarse.threads;
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapShader;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.PorterDuff;
+import android.graphics.PorterDuffXfermode;
+import android.graphics.Rect;
+import android.graphics.RectF;
 import android.graphics.Shader;
 import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
 import android.graphics.drawable.ShapeDrawable;
 import android.graphics.drawable.shapes.OvalShape;
 import android.os.AsyncTask;
+import android.view.Menu;
 import android.view.MenuItem;
 
+import androidx.core.graphics.drawable.RoundedBitmapDrawable;
+import androidx.core.graphics.drawable.RoundedBitmapDrawableFactory;
+
 import com.bumptech.glide.Glide;
+import com.example.tenarse.R;
 
 import java.util.concurrent.ExecutionException;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 public class LoadImageBottomNavBar extends AsyncTask<String, Void, Bitmap> {
     private MenuItem menuItem;
@@ -29,10 +42,13 @@ public class LoadImageBottomNavBar extends AsyncTask<String, Void, Bitmap> {
         String imageUrl = params[0];
         Bitmap bitmap = null;
         try {
+            int targetSize = (int) (context.getResources().getDisplayMetrics().density * 50);
             bitmap = Glide.with(context.getApplicationContext())
                     .asBitmap()
                     .load(imageUrl)
-                    .submit()
+                    .override(targetSize, targetSize)
+                    .centerCrop()
+                    .into(targetSize, targetSize)
                     .get();
         } catch (InterruptedException | ExecutionException e) {
             e.printStackTrace();
@@ -43,8 +59,30 @@ public class LoadImageBottomNavBar extends AsyncTask<String, Void, Bitmap> {
     @Override
     protected void onPostExecute(Bitmap bitmap) {
         if (bitmap != null) {
-            Drawable drawable = new BitmapDrawable(context.getResources(), bitmap);
+            Bitmap circularBitmap = getRoundedBitmap(bitmap);
+            Drawable drawable = new BitmapDrawable(context.getResources(), circularBitmap);
             menuItem.setIcon(drawable);
         }
+    }
+
+    private Bitmap getRoundedBitmap(Bitmap bitmap) {
+        Bitmap output = Bitmap.createBitmap(bitmap.getWidth(),
+                bitmap.getHeight(), Bitmap.Config.ARGB_8888);
+        Canvas canvas = new Canvas(output);
+        final int color = 0xff424242;
+        final Paint paint = new Paint();
+        final Rect rect = new Rect(0, 0, bitmap.getWidth(), bitmap.getHeight());
+        final RectF rectF = new RectF(rect);
+        final float roundPx = bitmap.getWidth() / 2f;
+
+        paint.setAntiAlias(true);
+        canvas.drawARGB(0, 0, 0, 0);
+        paint.setColor(color);
+        canvas.drawRoundRect(rectF, roundPx, roundPx, paint);
+
+        paint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.SRC_IN));
+        canvas.drawBitmap(bitmap, rect, rect, paint);
+
+        return output;
     }
 }
