5909f7780af5464bd79c5a3d591840122132b135
Argo <a19nargomcar@inspedralbes.cat>
2023-05-19 10:39:33 +0200

    Version FINAL Server 1.0

--- Server/database/delete.js
+++ Server/database/delete.js
@@ -9,17 +9,16 @@ const { Chat } = require('./connection')
 const readDB = require('./read')
 
 const deletePost = async function (idPost, username, callback) {
-    readDB.getPublicacio(idPost, (deletedPost) => {
+    await readDB.getPublicacio(idPost, (deletedPost) => {
         if (deletedPost.tipus == 'image') {
             const path_inicial = deletedPost.url_img;
-            const path_final = path_inicial.replace('http://localhost:3000', path.join(__dirname, '..', '')); //localhost:3000 -> 212.227.40.235
-            console.log(path_final);
+            const path_final = path_inicial.replace('https://tenarse.online/Tenarse/Server/', ""); //localhost:3000 -> 212.227.40.235
             fs.unlink(path_final, (err) => {
                 if (err) throw err;
             });
         } else if (deletedPost.tipus == 'video') {
             const path_inicial = deletedPost.url_video;
-            const path_final = path_inicial.replace('http://localhost:3000', path.join(__dirname, '..', ''));
+            const path_final = path_inicial.replace('https://tenarse.online/Tenarse/Server/', "");
             fs.unlink(path_final, (err) => {
                 if (err) throw err;
             });
